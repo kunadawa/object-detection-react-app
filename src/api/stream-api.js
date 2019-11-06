400589a224fb323e134395585905b2afe439ece4
Eric Njogu <kunadawa@gmail.com>
2019-11-06 15:06:44 +0300

    fix: using addEventListener instead of onmessage for registering callback
    credit - https://www.html5rocks.com/en/tutorials/eventsource/basics/

--- src/api/stream-api.js
+++ src/api/stream-api.js
@@ -1,10 +1,11 @@
 // TODO - load this as a configuration value
 const STREAM_PATH = 'http://localhost:5000/stream'
+const DETECTION_EVENT = 'detection'
 
 function register_stream_callback(callback) {
     // TODO will this be created every time this function is called?
     const source = new EventSource(STREAM_PATH)
-    source.onmessage = callback
+    source.addEventListener(DETECTION_EVENT, callback)
 }
 
 export {register_stream_callback};
