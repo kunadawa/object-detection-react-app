2f18fe7bbff8b84962436eb4228a7c46c6f5ad6d
Eric Njogu <kunadawa@gmail.com>
2019-11-28 18:27:35 +0300

    chore: sample images and event data for frontend-only testing

---
+++ src/test/sample-events.js
@@ -0,0 +1,230 @@
+exports.events = [
+{
+  "instanceName": "eric-aspire",
+  "source": "/home/mugo/Videos/train-passenger-foot-stuck.mp4",
+  "startTimestamp": 1574840832,
+  "frameCount": "165",
+  "detectionScores": [
+    0.8971184492111206
+  ],
+  "detectionClasses": [
+    1
+  ],
+  "detectionBoxes": {
+    "numbers": [
+      0.32574912905693054,
+      0.033033743500709534,
+      0.7953935861587524,
+      0.25357526540756226
+    ],
+    "shape": [
+      1,
+      4
+    ]
+  },
+  "stringMap": {
+    "frame_path": "/frames/91a68e3297ca7bcba592fd62f189f64119c0f2eab914bc1526131e37c5a4c1e7.jpg",
+    "id": "91a68e3297ca7bcba592fd62f189f64119c0f2eab914bc1526131e37c5a4c1e7"
+  },
+  "floatMap": {
+    "frame_height": 400,
+    "frame_width": 400
+  },
+  "categoryIndex": {
+    "1": "person"
+  },
+  "seen": true
+},
+{
+  "instanceName": "eric-aspire",
+  "source": "/home/mugo/Videos/train-passenger-foot-stuck.mp4",
+  "startTimestamp": 1574840832,
+  "frameCount": "195",
+  "detectionScores": [
+    0.8439833521842957
+  ],
+  "detectionClasses": [
+    1
+  ],
+  "detectionBoxes": {
+    "numbers": [
+      0.30109167098999023,
+      0.0014810636639595032,
+      0.8016583919525146,
+      0.1885722577571869
+    ],
+    "shape": [
+      1,
+      4
+    ]
+  },
+  "stringMap": {
+    "id": "05bf91777d5617c42789d2c38ef8a9475a2834198dff8064163d12b8e88ef9a7",
+    "frame_path": "/frames/05bf91777d5617c42789d2c38ef8a9475a2834198dff8064163d12b8e88ef9a7.jpg"
+  },
+  "floatMap": {
+    "frame_height": 400,
+    "frame_width": 400
+  },
+  "categoryIndex": {
+    "1": "person"
+  },
+  "seen": true
+},
+{
+  "instanceName": "eric-aspire",
+  "source": "/home/mugo/Videos/cow-boys.mp4",
+  "startTimestamp": 1574841216,
+  "frameCount": "15",
+  "detectionScores": [
+    0.9525127410888672
+  ],
+  "detectionClasses": [
+    1
+  ],
+  "detectionBoxes": {
+    "numbers": [
+      0.1274387538433075,
+      0.4404115080833435,
+      0.8686906099319458,
+      0.9547390341758728
+    ],
+    "shape": [
+      1,
+      4
+    ]
+  },
+  "stringMap": {
+    "frame_path": "/frames/42b593c573668f0f717b3f19dd118031ec62eb399bbf6a69c91bb75df97cac61.jpg",
+    "id": "42b593c573668f0f717b3f19dd118031ec62eb399bbf6a69c91bb75df97cac61"
+  },
+  "floatMap": {
+    "frame_width": 400,
+    "frame_height": 400
+  },
+  "categoryIndex": {
+    "1": "person"
+  },
+  "seen": true
+},
+{
+  "instanceName": "eric-aspire",
+  "source": "/home/mugo/Videos/cow-boys.mp4",
+  "startTimestamp": 1574841216,
+  "frameCount": "220",
+  "detectionScores": [
+    0.8022869229316711,
+    0.8014155626296997
+  ],
+  "detectionClasses": [
+    1,
+    1
+  ],
+  "detectionBoxes": {
+    "numbers": [
+      0.07242226600646973,
+      0.474941611289978,
+      0.5095248818397522,
+      0.7710906267166138,
+      0.08440393209457397,
+      0.5277665257453918,
+      0.8214264512062073,
+      0.9941529631614685
+    ],
+    "shape": [
+      2,
+      4
+    ]
+  },
+  "stringMap": {
+    "frame_path": "/frames/aa6c2d0ca36e583970176218a678c07689df675a07444e0ea34f19fc9d34e758.jpg",
+    "id": "aa6c2d0ca36e583970176218a678c07689df675a07444e0ea34f19fc9d34e758"
+  },
+  "floatMap": {
+    "frame_width": 400,
+    "frame_height": 400
+  },
+  "categoryIndex": {
+    "1": "person"
+  },
+  "seen": true
+},
+{
+  "instanceName": "eric-aspire",
+  "source": "/home/mugo/Videos/train-passenger-foot-stuck.mp4",
+  "startTimestamp": 1574840832,
+  "frameCount": "935",
+  "detectionScores": [
+    0.7449436187744141
+  ],
+  "detectionClasses": [
+    1
+  ],
+  "detectionBoxes": {
+    "numbers": [
+      0.31709006428718567,
+      0.4069455564022064,
+      0.8288698196411133,
+      0.6015183925628662
+    ],
+    "shape": [
+      1,
+      4
+    ]
+  },
+  "stringMap": {
+    "frame_path": "/frames/d2c847eb823316d15b086454fe0dd7d32ffad628f0c67e16b866975ee32a633d.jpg",
+    "id": "d2c847eb823316d15b086454fe0dd7d32ffad628f0c67e16b866975ee32a633d"
+  },
+  "floatMap": {
+    "frame_width": 400,
+    "frame_height": 400
+  },
+  "categoryIndex": {
+    "1": "person"
+  },
+  "seen": true
+},
+{
+  "instanceName": "eric-aspire",
+  "source": "/home/mugo/Videos/cow-boys.mp4",
+  "startTimestamp": 1574841216,
+  "frameCount": "695",
+  "detectionScores": [
+    0.8644493818283081,
+    0.7225265502929688
+  ],
+  "detectionClasses": [
+    19,
+    1
+  ],
+  "detectionBoxes": {
+    "numbers": [
+      0.2383696287870407,
+      0.26213979721069336,
+      0.5603454113006592,
+      0.6264521479606628,
+      0.13030800223350525,
+      0.5070068836212158,
+      0.5160547494888306,
+      0.6714723110198975
+    ],
+    "shape": [
+      2,
+      4
+    ]
+  },
+  "stringMap": {
+    "id": "4aa9f8a75142e211f1acba1bf88c9e67bf6748a93b4e65ea932501b223cc619b",
+    "frame_path": "/frames/4aa9f8a75142e211f1acba1bf88c9e67bf6748a93b4e65ea932501b223cc619b.jpg"
+  },
+  "floatMap": {
+    "frame_height": 400,
+    "frame_width": 400
+  },
+  "categoryIndex": {
+    "1": "person",
+    "19": "horse"
+  },
+  "seen": true
+}];
