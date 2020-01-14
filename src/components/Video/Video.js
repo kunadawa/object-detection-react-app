4c5a6b70e3e93c529525ec85bba0fe00cca23ecc
Eric Njogu <kunadawa@gmail.com>
2020-01-14 15:06:24 +0300

    test: using updateend event to know when to update the source buffer
    The video is still not streaming

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -24,16 +24,29 @@ class Video extends Component {
     // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Video_codecs
     // https://developer.mozilla.org/en-US/docs/Web/API/ReadableStreamDefaultReader
     // https://developers.google.com/web/fundamentals/media/mse/basics
-    fetch ("http//:localhost:50644").then(response => {
+    // https://stackoverflow.com/questions/20042087/could-not-append-segments-by-media-source-api-get-invalidstateerror-an-attem
+    fetch ("http://localhost:50644").then(response => {
       const mediaSource = event.target
+      const queue = []
       const videoSourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp9"')
+      videoSourceBuffer.addEventListener('updateend', (event) => {
+        if (queue.length) {
+          videoSourceBuffer.appendBuffer(queue.shift())
+        }
+      })
       const reader = response.body.getReader();
+      let firstAppend = true;
       function push() {
         return reader.read().then(({done, value}) => {
           if (done) {
             return;
           }
-          videoSourceBuffer.appendBuffer(value)
+          if (firstAppend) {
+            videoSourceBuffer.appendBuffer(value)
+            firstAppend = false
+          } else {
+            queue.push(value)
+          }
         }).then(push)
       }
       push();
