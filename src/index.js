3b789c0c4464cbe14ab8959e7de5ba81018d9ebe
Eric Njogu <kunadawa@gmail.com>
2020-01-17 11:11:09 +0300

    chore: reinstating the App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './components/App/App';
-import Video from './components/Video/Video'
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<Video />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
